a3d04f36858e2c185362a8044dff0ff1a7cdbda9
MikeDengfei <32149201+MikeDengfei@users.noreply.github.com>
2018-12-02 16:40:35 +0800

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 from api.db_test import db_add
 from api.login import login, login_out
-from api.user import register, change_permission, change_disable, query_user
+from api.user import register, change_permission, change_disable, query_user, query_user2
 from api.department import add_department, delete_department, query_department
 from api.period_data import add_period_data, delete_period_data, query_period_data
 from api.court_resource import add_court_resource, delete_court_resource, query_court_resource
@@ -49,5 +49,6 @@ __all__ = [
     'period_edit',
     'court_type_query',
     'sportsfield_delete'
-    'period_delete'
+    'period_delete',
+    'query_user2'
 ]
